2e0d8f2a786613dd6e4759db9a1c97db54364ab6
Anuj Kumar <136914209+Ak-Anuj@users.noreply.github.com>
2026-03-02 09:56:16 +0530

    Update firebase.js

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,14 +2,16 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
+
 const firebaseConfig = {
-  apiKey: "import.meta.env.VITE_FIREBASE_APIKEY",
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: "examnotes-e4f82.firebaseapp.com",
   projectId: "examnotes-e4f82",
-  storageBucket: "examnotes-e4f82.firebasestorage.app",
+  storageBucket: "examnotes-e4f82.appspot.com",
   messagingSenderId: "925341590102",
-  appId: "1:925341590102:web:925d9a427e10e25c2d11f7",
+  appId: "1:925341590102:web:925d9a427e10e25c2d11f7"
 };
+
 
 
 const app = initializeApp(firebaseConfig);
